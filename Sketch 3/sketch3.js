85967f59cd8b58723c915319adb39156da1a3fc5
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-13 12:11:07 -0400

    Sound Not Working

--- Sketch 3/sketch3.js
+++ Sketch 3/sketch3.js
@@ -1,15 +1,33 @@
+var mySound;
+
 function setup(){
+	mySound = createAudio("sound.mp3");
+	bubble = {
+		x: width/2,
+		y: height/2
+		r: 15
+	};
 	createCanvas(720, 480);
 }
 
 function draw(){
-	ellipse(width/2, height/2, 30);
+	ellipse(bubble.x, bubble.y, bubble.r * 2);
+	bubble.x += random(-5, 5);
+	bubble.y += random(-5, 5);
+	if(bubble.x > width || bubble.x < 0){
+		bubble.x = width/2;
+		bubble.y = height/2;
+	}
+	if(bubble.y > height || bubble.y < 0){
+		bubble.y = height/2;
+		bubble.x = width/2;
+	}
 }
 
 function mouseClicked(){
-	if(mouseX < width/2 + 15 && mouseX > width/2 - 15){
-		if(mouseY < height/2 + 15 && mouseY > height/2 - 15){
-			fill(100);
-		}
+	var clickDistance = dist(width/2, height/2, mouseX, mouseY);
+	if(clickDistance < bubble.r){
+		fill(100);
+		mySound.play();
 	}
 }
