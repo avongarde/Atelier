97262746b7f5ae67a106302231ce0f220fbeed64
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-13 10:53:33 -0400

    Test

--- Sketch 2/sketch2.js
+++ Sketch 2/sketch2.js
@@ -7,15 +7,16 @@ function setup(){
 function draw(){
 	for(var i = 0; i < mouseClick.length; i++){
    		ellipse(mouseClick[i].x, mouseClick[i].y, 10);
-   		mouseClick[i].x++;
-   		mouseClick[i].y++;
+   		mouseClick[i].x += mouseClick[i].speed;
+   		mouseClick[i].y += mouseClick[i].speed;
 	}
 }
 
 function mouseClicked(){
 	var clickedPosition = {
 		x: mouseX,
-		y: mouseY
+		y: mouseY,
+		speed: random(-2, 2)
 	};
 	mouseClick.push(clickedPosition);
 }
