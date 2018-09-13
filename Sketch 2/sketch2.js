381fa60ea77a79ebe0d95ad49a67ddffad6c8871
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-13 11:40:11 -0400

    Final Sketch 2 - Not working

--- Sketch 2/sketch2.js
+++ Sketch 2/sketch2.js
@@ -2,25 +2,35 @@ var mouseClick = [];
 
 function setup(){
     createCanvas(720, 480);
+    var button = createButton('Click Here');
+    button.mousePressed(clickHere);
 }
 
 function draw(){
+	//translate(width/2, height/2);
 	for(var i = 0; i < mouseClick.length; i++){
 		//colorMode(HSB, 100);
-		//fill(random(360), random(100), random(100), random(1));
-		fill(random(255), random(255), random(255), random(255));
-   		ellipse(mouseClick[i].x, mouseClick[i].y, 10);
-   		mouseClick[i].x += mouseClick[i].speed;
-   		mouseClick[i].y += mouseClick[i].speed;
+		//fill(random(360), random(100), random(100), random(1));	
+		fill(mouseClick[i].color);
+		ellipse(mouseClick[i].x, mouseClick[i].y, 10);
+   		mouseClick[i].x += mouseClick[i].xSpeed;
+   		mouseClick[i].y += mouseClick[i].ySpeed;
 	}
 }
 
-function mouseDragged(){
+//function mouseDragged(){
+function clickHere(){
 	var clickedPosition = {
 		x: mouseX,
 		y: mouseY,
-		speed: random(-2, 2),
-		
+		xSpeed: random(-2, 2),
+		ySpeed: random(-2, 2),
+		color: color(random(100, 150), random(255), random(255))  	
 	};
 	mouseClick.push(clickedPosition);
+}
+
+function mouseClicked(){
+	createP("HELLO");
+	//createA()
 }
