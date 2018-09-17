c3470eea3f060f938fef9bc307a538db7a7052c2
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-17 17:41:28 -0400

    Filters Final (Not Working)

--- Sketch 2/sketch2.js
+++ Sketch 2/sketch2.js
@@ -1,17 +1,28 @@
 // references video input example
 // https://p5js.org/examples/dom-video-capture.html
-
 var capture;
 
 function setup() {
-  createCanvas(390, 240);
+  createCanvas(window.innerWidth, window.innerHeight);
   capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+  capture.size(480, 320);
+  capture.hide();
 }
 
 function draw() {
   background(255);
-  image(capture, 0, 0, 320, 240);
+  push();
+  image(capture, 0, 0);
   filter('INVERT');
+  pop();
+  image(capture, width/3, 0);
+  // filter('OPAQUE');
+  image(capture, width/3 * 2, 0);
+  // filter('POSTERIZE');
+  image(capture, 0, height/2);
+  // filter('GRAY');
+  image(capture, width/3, height/2);
+  // filter('ERODE');
+  image(capture, width/3 * 2, height/2);
+  filter('DILATE');
 }
