b61ef55772bd5f36cef9c036913bdc7506c1bf66
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-17 15:29:52 -0400

    Fireworks Final

--- Sketch 3/sketch3.js
+++ Sketch 3/sketch3.js
@@ -1,34 +1,40 @@
-var mySound;
+var circles = [];
 
 function setup(){
-	mySound = createAudio('sound.mp3');
-	bubble = {
-		x: width/2,
-		y: height/2,
-		r: 15
-	};
-	createCanvas(720, 480);
+	createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw(){
-	ellipse(bubble.x, bubble.y, bubble.r * 2);
-	bubble.x += random(-5, 5);
-	bubble.y += random(-5, 5);
-	if(bubble.x > width || bubble.x < 0){
-		bubble.x = width/2;
-		bubble.y = height/2;
-	}
-	if(bubble.y > height || bubble.y < 0){
-		bubble.y = height/2;
-		bubble.x = width/2;
+	background(0, 0, 0, 100);
+	for (var i = 0; i < circles.length; i++){
+		noStroke();
+		circles[i].lifespan += 0.1;
+		circles[i].alpha -= circles[i].lifespan;
+		fill(circles[i].r, circles[i].g, circles[i].b, circles[i].alpha);
+		ellipse(circles[i].x, circles[i].y, 10, 10);
+		circles[i].y += circles[i].ySpeed;
+		circles[i].x += circles[i].xSpeed;
+		circles[i].ySpeed += 0.1;
 	}
 }
 
 function mouseClicked(){
-	var clickDistance = dist(bubble.x, bubble.y, mouseX, mouseY);
-	if(clickDistance < bubble.r){
-		fill(100);
-		mySound.play();
-		console.log(mySound);
+	var r = random(255);
+	var g = random(255);
+	var b = random(255);
+
+	for(var i = 0; i < 50; i++){
+		var particle = {
+			x: mouseX,
+			y: mouseY,
+			xSpeed: random(-3, 3),
+			ySpeed: random(-3, 3),
+			lifespan: 0,
+			alpha: 255,
+			r: r,
+			g: g,
+			b: b
+		}
+	circles.push(particle);
 	}
 }
