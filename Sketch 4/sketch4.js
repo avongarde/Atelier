735c2d9a7462b198d7b175c57284630533875927
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-17 16:19:56 -0400

    Panning Final

--- Sketch 4/sketch4.js
+++ Sketch 4/sketch4.js
@@ -1,44 +1,59 @@
-// stolen from example
-// https://p5js.org/reference/#/p5.FFT
+// references sketch 3 & Pan Sound example
+// https://p5js.org/examples/sound-pan-sound.html
 
-var waveform;
+var sound;
+var circles = [];
 
 function preload(){
   sound = loadSound('sound.mp3');
+  console.log(sound);
 }
 
 function setup(){
-  var cnv = createCanvas(720, 480);
+  var cnv = createCanvas(window.innerWidth, window.innerHeight);
   cnv.mouseClicked(togglePlay);
-  fft = new p5.FFT();
+  console.log(togglePlay);
   sound.amp(0.2);
 }
 
 function draw(){
   background(0);
-
-  var spectrum = fft.analyze();
-  noStroke();
-  fill(0,255,0); // spectrum is green
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
+  for (var i = 0; i < circles.length; i++){
+    noStroke();
+    circles[i].lifespan += 0.1;
+    circles[i].alpha -= circles[i].lifespan;
+    fill(circles[i].r, circles[i].g, circles[i].b, circles[i].alpha);
+    ellipse(circles[i].x, circles[i].y, 10, 10);
+    circles[i].y += circles[i].ySpeed;
+    circles[i].x += circles[i].xSpeed;
+    circles[i].ySpeed += 0.1;
   }
+}
 
-  waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(255,255,0); // waveform is red
-  strokeWeight(1);
-  for (var i = 0; i< waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
+function mouseMoved(){
+  var r = random(255);
+  var g = random(255);
+  var b = random(255);
+  var panning = map(mouseX, 0., width, -1.0, 1.0);
+  
+  sound.pan(panning);
+  // sound.play();
+
+  for(var i = 0; i < 50; i++){
+    var particle = {
+      x: mouseX,
+      y: mouseY,
+      xSpeed: random(-3, 3),
+      ySpeed: random(-3, 3),
+      lifespan: 0,
+      alpha: 100,
+      r: r,
+      g: g,
+      b: b
+    }
+  circles.push(particle);
   }
-  endShape();
-
-  text('click to play/pause', 4, 10);
+  return false;
 }
 
 // fade sound if mouse is over canvas
