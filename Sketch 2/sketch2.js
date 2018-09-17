68a0dbd63a185d1ce81e4d321d9c293ee0523232
Francisco Samayoa <3165936@student.ocadu.ca>
2018-09-17 15:30:03 -0400

    Video Input Test

--- Sketch 2/sketch2.js
+++ Sketch 2/sketch2.js
@@ -1,36 +1,17 @@
-var mouseClick = [];
+// references video input example
+// https://p5js.org/examples/dom-video-capture.html
 
-function setup(){
-    createCanvas(720, 480);
-    var button = createButton('Click Here');
-    button.mousePressed(clickHere);
+var capture;
+
+function setup() {
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
-function draw(){
-	//translate(width/2, height/2);
-	for(var i = 0; i < mouseClick.length; i++){
-		//colorMode(HSB, 100);
-		//fill(random(360), random(100), random(100), random(1));	
-		fill(mouseClick[i].color);
-		ellipse(mouseClick[i].x, mouseClick[i].y, 10);
-   		mouseClick[i].x += mouseClick[i].xSpeed;
-   		mouseClick[i].y += mouseClick[i].ySpeed;
-	}
-}
-
-//function mouseDragged(){
-function clickHere(){
-	var clickedPosition = {
-		x: mouseX,
-		y: mouseY,
-		xSpeed: random(-2, 2),
-		ySpeed: random(-2, 2),
-		color: color(random(100, 150), random(255), random(255))  	
-	};
-	mouseClick.push(clickedPosition);
-}
-
-function mouseClicked(){
-	createP("HELLO");
-	//createA()
+function draw() {
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter('INVERT');
 }
